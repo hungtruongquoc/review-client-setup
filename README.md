67abf1c88bc0d53367f5c8a02d09b5b3f08df642
Hung Truong <hungtruongquoc@gmail.com>
2020-11-11 22:39:53 -0600

    20201111: Added new code for sending message to SQS

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # review-client-setup
 # References
 [Create Python Lambda with Serverless Framework](https://medium.com/faun/aws-lambda-serverless-framework-python-part-1-a-step-by-step-hello-world-4182202aba4a)
+
+[Create Python Lambda](https://medium.com/faun/aws-lambda-serverless-framework-python-part-1-a-step-by-step-hello-world-4182202aba4a)
+
 [LocalStack serverless example](https://github.com/localstack/serverless-examples)
+
 [LocalStack](https://github.com/localstack/localstack)
+
 [Dask Example](https://github.com/timothymugayi/localstack_python_examples)
--- infrastructure/serverless/new_user/app.py
+++ infrastructure/serverless/new_user/app.py
@@ -1,5 +1,6 @@
 import json
 import flask
+import boto3
 
 app = flask.Flask(__name__)
 
@@ -10,4 +11,29 @@ def health_check():
 @app.route("/register", methods = ['GET', 'POST'])
 def sns():
     #print(flask.request.json)
+    sqs = boto3.client('sqs')
+    queue_url = 'https://sqs.us-east-2.amazonaws.com/723567309652/TenantDbCreateQueue'
+    response = sqs.send_message(
+        QueueUrl=queue_url,
+        DelaySeconds=10,
+        MessageAttributes={
+            'Title': {
+                'DataType': 'String',
+                'StringValue': 'The Whistler'
+            },
+            'Author': {
+                'DataType': 'String',
+                'StringValue': 'John Grisham'
+            },
+            'WeeksOn': {
+                'DataType': 'Number',
+                'StringValue': '6'
+            }
+        },
+        MessageBody=(
+            'Information about current NY Times fiction bestseller for '
+            'week of 12/11/2016.'
+        )
+    )
+    print(response['MessageId'])
     return flask.jsonify(flask.request.json)
